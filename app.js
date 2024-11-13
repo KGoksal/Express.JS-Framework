9ced2f5cdf4d13404ad0b0f3acca2bbcd7a65f44
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-11-12 20:22:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-// I imported the Express framework and created an Express application
+// Import the Express framework and create an Express application
 const express = require('express');
 const app = express();
 const port = 3000;
-app.set('view engine', 'ejs'); // I set the view engine to EJS
+app.set('view engine', 'ejs'); // Set the view engine to EJS
 
-// I created a basic route for the homepage I made this route respond with an EJS template when accessing the root URL
+// Basic route for the homepage that renders an EJS template
 app.get('/', (req, res) => {
-  res.render('index', { title: "My EJS App!"});
+  res.render('index', { title: "My EJS App!!"});
 });
 
-// I created a route with URL parameters. I made this route capture the ID parameter from the URL
-
+// Route with URL parameters that captures and displays the ID parameter
 app.get('/user/:id', (req, res) => {
   const param = req.params.id;
-  res.send(`The parameter of ID is: ${param}`); // made this route display the ID parameter
+  res.send(`The parameter of ID is: ${param}`);
 });
 // Example: /user/123 will display "The parameter of ID is: 123"
 
+// Route for contact page
+app.get('/contact', (req, res) => {
+  res.render('contactme', { title: "Contact Me!"});
+});
 
-// I created a route that handles query parameters, I made this route read the 'user' query parameter from the URL
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard', { title: "Dashboard", username: "J.F. Kadir "});
+});
 
+// Route that handles and displays query parameters from the URL
 app.get('/admin', (req, res) => {
   const queryparam = req.query.user;
-  res.send(`The query parameter of user is: ${queryparam}`); // made this route display the user query parameter
+  res.send(`The query parameter of user is: ${queryparam}`);
 });
 // Example: /admin?user=john will display "The query parameter of user is: john"
-
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
